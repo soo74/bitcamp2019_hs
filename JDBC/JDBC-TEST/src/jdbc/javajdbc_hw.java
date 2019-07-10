70022757cf9170efa0af3c2f33b3d381668dddc4
HUISOO <heuisu704@gmail.com>
2019-07-10 18:28:03 +0900

    java jdbc 과제

--- JDBC/JDBC-TEST/src/jdbc/javajdbc_hw.java
+++ JDBC/JDBC-TEST/src/jdbc/javajdbc_hw.java
@@ -9,7 +9,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class javajdbc_hw {
+public class javajdbc_hw  {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException{
 		
