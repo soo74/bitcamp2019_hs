9dc4bcba69c1f53b5f26e2ed070f0c49c4e7d64f
soo74 <soo74@gmail.com>
2019-09-24 13:18:25 +0900

    문자열 데이터 저장

--- Nodejs/testFsWrite.js
+++ Nodejs/testFsWrite.js
@@ -1,8 +1,19 @@
 var fs = require('fs');
-fs.writeFile('textData.txt', 'Hello World1234', function(err) {       //Hello World1234으로 변경하면 textData.txt에서  Hello World -> Hello World1234로 변경
-if ( err ) {
-console.error('파일 저장 실패 : ', err);
-return;
-}
-console.log('파일 저장 성공');
-});
+
+var path = 'textData.txt';
+
+fs.appendFile(path, '\n Additional data', function(err) {    //'Additional data'이 textData.txt에 추가된다.
+    if ( err ) {
+    console.error('파일 내용 추가 실패 : ', err);
+    }
+    console.log('파일 내용 추가 성공');
+    });
+
+
+//fs.writeFile('textData.txt', 'Hello World1234', function(err) {       //Hello World1234으로 변경하면 textData.txt에서  Hello World -> Hello World1234로 변경
+//if ( err ) {
+//console.error('파일 저장 실패 : ', err);
+//return;
+//}
+//console.log('파일 저장 성공');
+//});
