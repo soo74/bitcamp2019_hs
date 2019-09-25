184f79e040aac1493effbb6646a6021dcf4b57ef
soo74 <soo74@gmail.com>
2019-09-25 16:53:15 +0900

    Revert "이미지,html 출력"
    This reverts commit ca9807554674cc4885a727b65f4171b407fddad3.

--- Nodejs/testURL.js
+++ Nodejs/testURL.js
@@ -5,5 +5,5 @@ var urlStr = 'http://idols.com/q?group=EXID&name=하니&since=';
 
 var parsed = url.parse(urlStr);
 
-//console.log(parsed);
-console.log(parsed.host);
+console.log(parsed);
+console.log(parsed).host;
