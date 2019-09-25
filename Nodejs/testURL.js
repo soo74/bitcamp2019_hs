868be011697644f9c088804e98899a918db4b02c
soo74 <soo74@gmail.com>
2019-09-25 10:24:34 +0900

    ppt모듈3_URL

---
+++ Nodejs/testURL.js
@@ -0,0 +1,9 @@
+//URL 모듈 로드
+var url = require('url');
+
+var urlStr = 'http://idols.com/q?group=EXID&name=하니&since=';
+
+var parsed = url.parse(urlStr);
+
+console.log(parsed);
+console.log(parsed).host;
